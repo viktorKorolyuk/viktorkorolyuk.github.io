8588b5beb50a934617167c9bd6b897964c53b52c
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2019-11-01 21:08:22 -0400

    Updated formatting of main.js

--- res/js/main.js
+++ res/js/main.js
@@ -53,32 +53,32 @@ pgHandler.options = {
 };
 
 pgHandler.gallery = [{
-    name:"Animated point mesh",
-    desc:"Spawns a random set of moving points on the screen and connects them with lines",
-    url:"https://codepen.io/ViktorKorolyuk/full/QWLmWPm"
-},{
-    name:"Conway's Game of Life",
-    desc:"A colourful implementation of the popular cellular automaton by John Horton Conway",
-    url:"https://codepen.io/ViktorKorolyuk/full/dJZPGK"
-},{
-    name:"JS data-structs",
-    desc:"A set of simple data structures which parse into JSON",
-    url:"https://github.com/viktorKorolyuk/JS-Data-Structures"
-},{
-    name:"Electron Music Player",
-    desc:"Cross-platform music player made with electron",
-    url:"https://github.com/viktorKorolyuk/Fancy-Music-Player"
-},{
-    name:"Not-Latex",
-    "desc":"A latex-inspired javascript text processor",
-    url:"https://github.com/viktorKorolyuk/Not-Latex"
+    name: "Animated point mesh",
+    desc: "Spawns a random set of moving points on the screen and connects them with lines",
+    url: "https://codepen.io/ViktorKorolyuk/full/QWLmWPm"
+}, {
+    name: "Conway's Game of Life",
+    desc: "A colourful implementation of the popular cellular automaton by John Horton Conway",
+    url: "https://codepen.io/ViktorKorolyuk/full/dJZPGK"
+}, {
+    name: "JS data-structs",
+    desc: "A set of simple data structures which parse into JSON",
+    url: "https://github.com/viktorKorolyuk/JS-Data-Structures"
+}, {
+    name: "Electron Music Player",
+    desc: "Cross-platform music player made with electron",
+    url: "https://github.com/viktorKorolyuk/Fancy-Music-Player"
+}, {
+    name: "Not-Latex",
+    "desc": "A latex-inspired javascript text processor",
+    url: "https://github.com/viktorKorolyuk/Not-Latex"
 
-},{
+}, {
     name: "SWipe",
     desc: "Are you fast enough?",
     url: "https://viktorkorolyuk.github.io/SWipe/",
     img: "SWipe-game.png"
-},{
+}, {
     name: "Codepen.io",
     desc: "A link to my codepen.io 'portfolio'",
     url: "https://codepen.io/ViktorKorolyuk/"
